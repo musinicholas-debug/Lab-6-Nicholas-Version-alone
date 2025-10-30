e719ae02f85a04915226ae61a3b4e44909df027c
musinicholas-debug <musinicholas@gmail.com>
2025-10-30 15:48:14 -0400

    Fixed phrases structure for all answers (added proper puntuation, etc)

--- Lab 6.py
+++ Lab 6.py
@@ -1,7 +1,6 @@
 #Part 3
 import pandas as pd
 import seaborn as sns
-import numpy as np
 df=pd.read_csv("wdi_wide.csv")
 
 #Question 3
@@ -42,42 +41,43 @@ for i in filtered_df["Country Name"] :
 #Question 1
 sns.relplot(data=df, x="Life expectancy, male", y="GNI per capita")
 sns.relplot(data=df, x="Life expectancy, female", y="GNI per capita")
-#yes the longer you live the more GNI per Capita you have 
+#Yes the longer you live the more GNI per Capita you have 
 
 #Question 2
 sns.relplot(data=df, x="Life expectancy, male", y="GNI per capita", hue="Region")
 sns.relplot(data=df, x="Life expectancy, female", y="GNI per capita", hue="Region")
+#As seen by the plot, the most developed regions have longer life expectancies, and therefore, they have higher GNI per capita.
 
 
 #Question 3 
 sns.relplot(data=df, kind="line", x="Life expectancy, female", y="GNI per capita", hue="Region", errorbar="sd")
-
+#What is noticeable for this plot is that even if I included the standard deviation for the plot, it is unnoticeable.
 
 #Question 4 
 sns.lmplot(data=df, x="Life expectancy, female", y="GNI per capita", hue="Region")
-
+#We notice that regions such as Africa have very small linear regressions compared to Europe, which has a very steep slope, which is also situated on the leftmost side of the x-axis, compared to the African one situated on the rightmost side of the x-axis. Showing a clear difference between life expectancy and the accumulation of money.
 
 #Question 5 
 
 #Do greenhouse gas emissions in each region influence women’s life expectancy?
 sns.relplot(data=df, x="Life expectancy, female", y="Greenhouse gas emissions", col="Region")
-#In each given region there is no real difference. For most countries the Greenhouse gas emission are relativately simalery to one another but female life expectancy still has a great difference espically in regions like africa we're certain contries have very low life expectancys and others very high despite having simalar Greenhouse gas emissions.
+#In each given region, there is no real difference. For most countries, the Greenhouse gas emissions are relatively similar to one another, but life expectancy still has a great difference, especially in regions like Africa we're certain countries have very low life expectancies and others very high, despite having similar Greenhouse gas emissions
 
 #For a given region, is there a link between women’s tertiary education and their life expectancy?
 sns.relplot(data=df, x="Life expectancy, female", y="Tertiary education, female", col="Region")
-#For this question the plots clearly demonstrate that the more females attend tertiarty education the longer they live. In every plot for each region this trend is noticable.
+#For this question, the plots clearly demonstrate that the more females attend tertiary education, the longer they live. In every plot for each region, this trend is noticeable.
 
 #Does access to physicians impact women’s life expectancy in a given region?
 sns.relplot(data=df, x="Life expectancy, female", y="Physicians", col="Region")
-#Once again the plots for each region with africa as the only outlier demonstrate that the more access a population has to physicans the longer in this specefic plots the women live. For africa in the ranges of life expectancy from 55 to 75 there is very little access to physicans so no correlation can be made.
+#Once again, the plots for each region with Africa as the only outlier demonstrate that the more access a population has to physicians, the longer in these specific plots the women live. For Africa, in the range of life expectancy from 55 to 75, there is very little access to physicians, so no correlation can be made.
 
 #How does life expectancy for women relate to Internet use in each region?
 sns.relplot(data=df, x="Life expectancy, female", y="Internet use", col="Region")
-#Again in countries with high internet use female life expectancy is high this can logically attributed with high internet use meaning a highly developped country meaning a higher life expectancy because of good social and medical infrastructre
+#Again, in countries with high internet use, female life expectancy is high. This can be logically attributed to high internet use, meaning a highly developed country, resulting in a higher life expectancy due to good social and medical infrastructure.
 
 #What is the relationship between women in national parliament and their life expectancy for each region?
 sns.relplot(data=df, x="Life expectancy, female", y="Women in national parliament", col="Region")
-#Similary to question 1 of Part 4 Q5 no real correlation can be made the points for each region are very random indicating no real relationship.
+#Similarly to question 1 of Part 4 Q5, no real correlation can be made; the points for each region are very random, indicating no real relationship.
 
 #Question 6 
 
@@ -91,11 +91,11 @@ filtered_df = df[df["Emissions per capita"] > 0.03]
 print(filtered_df)
 for i in filtered_df["Country Name"] :
         print(i)
-#There are only 2 countries that fit the criterias for a high emission countries
+#There are only 2 countries that fit the criterias for high-emission countries.
 
 #Part C
 sns.relplot(data=filtered_df, x="Internet use", y="Country Name",)
-#There a strong correlation because more then 95% of both Quatar and Brunei Darussalam populations use the internet
+#There is a strong correlation because more than 95% of both Qatar and Brunei Darussalam populations use the internet.
 
 #Part D
 filtered_df2 = df[df["High Income Economy"] > 0.03]
@@ -104,7 +104,8 @@ for i in filtered_df2["Country Name"] :
         print(i)
         
 sns.relplot(data=filtered_df2, x="High Income Economy", y="Emissions per capita",)
-# No not all high income economies have high emissions after seeing the plot you can see that many countries fall below the 0.03 high emission cutoff and still are high income economies.
+#No, not all high-income economies have high emissions. After seeing the plot, you can see that many countries fall below the 0.03 high emission cutoff and are still high-income economies.
+
 
         
 
